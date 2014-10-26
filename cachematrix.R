12d96ba50e7f261d199086a89d3ae75c08fc060d
williamstj <williamstj@btinternet.com>
2014-10-26 17:43:01 +0000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The purpose of these functions is (if the contents of a matrix are not changing)
+# is to cache the value of the inverse so that when we need it again, 
+# it can be looked up in the cache rather than recomputed. 
 
-## Write a short comment describing this function
+# The first function, makeCacheMatrix creates a special "vector", which is really a list containing a function to
+        # set the inverse of the matrix
+        # get the inverse of the matrix
+        # set the value of the inverse
+        # get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        m <- list()
+        length(m) <- length(x)
+        dim(m) <- dim(x)
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                length(m) <- length(x)
+                dim(m) <- dim(x)
+                m <- NULL
+        }
+        
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse
+             )     
+                
 }
 
-
-## Write a short comment describing this function
+# The following function calculates the inverse using the special "vector" created 
+# with the above function. However, it first checks to see if the inverse has 
+# already been calculated. If so, it gets the inverse from the cache and skips the 
+# computation. Otherwise, it calculates the inverse and sets the value of the 
+# inverse in the cache via the setinverse function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
